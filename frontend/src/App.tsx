beaf63de429d6b6f2ee562c9d45e18008cf5c860
Ryan Crabbe <ryancrabbe@Ryans-MBP.attlocal.net>
2024-08-29 01:53:41 -0700

    Integrated the google maps API and added a maps embed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { getTournaments } from "./services/tournamentService";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 function App() {
-  const [coordinates, setCoordinates] = useState("37.75439, -121.91937");
+  const [coordinates, setCoordinates] = useState("32.7157, -117.1611");
   const [tournaments, setTournaments] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  const mapContainerStyle = {
+    width: "100vw",
+    height: "400px",
+  };
+
+  const center = {
+    lat: 33.7454725,
+    lng: -117.86765300000002,
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY!,
+  });
+
   const fetchTournaments = async () => {
     setLoading(true);
-    setError(null); // Reset error state before new request
+    setError(null);
 
     try {
       const data = await getTournaments(coordinates);
-      console.log("Fetched tournaments:", data); // For debugging purposes
+      console.log("Fetched tournaments:", data);
       setTournaments(data);
     } catch (err) {
       console.error("Error fetching tournaments:", err);
@@ -25,8 +40,34 @@ function App() {
   };
 
   useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setCoordinates(`${latitude},${longitude}`);
+            setError(null);
+          },
+          (error) => {
+            setError("Unable to retrieve your location");
+            console.error(error);
+          }
+        );
+        console.log(coordinates);
+      } else {
+        setError("Geolocation is not supported by your browser");
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  useEffect(() => {
     fetchTournaments();
   }, [coordinates]);
+
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading Maps...</div>;
 
   return (
     <>
@@ -43,6 +84,11 @@ function App() {
             </li>
           ))}
         </ul>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={10}
+          center={center}
+        ></GoogleMap>
       </div>
     </>
   );
